9319cc49a28762b0ba4835437c70e3f15ba6fc63
Karoen van Hoog <karoen.vanhoog@nepgroup.com>
2022-02-19 22:12:07 +0100

    replaces tabs for spaces

--- mutesync.go
+++ mutesync.go
@@ -1,70 +1,70 @@
 package mutesync
 
 import (
-	"encoding/json"
-	"errors"
-	"io/ioutil"
-	"net/http"
-	"strconv"
+    "encoding/json"
+    "errors"
+    "io/ioutil"
+    "net/http"
+    "strconv"
 )
 
 const Port int = 8249
 
 func GetStatus(host, token string) (status Status, err error) {
-	var statusResponse statusResponse
+    var statusResponse statusResponse
 
-	req, err := http.NewRequest(http.MethodGet, "http://" + host + ":" + strconv.Itoa(Port) + "/state", nil)
-	if err != nil {
-		return statusResponse.Status, err
-	}
+    req, err := http.NewRequest(http.MethodGet, "http://"+host+":"+strconv.Itoa(Port)+"/state", nil)
+    if err != nil {
+        return statusResponse.Status, err
+    }
 
-	req.Header.Set("Authorization", "Token " + token)
-	req.Header.Set("x-mutesync-api-version", "1")
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return statusResponse.Status, err
-	}
+    req.Header.Set("Authorization", "Token "+token)
+    req.Header.Set("x-mutesync-api-version", "1")
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil {
+        return statusResponse.Status, err
+    }
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return statusResponse.Status, err
-	}
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return statusResponse.Status, err
+    }
 
-	err = json.Unmarshal(body, &statusResponse)
-	if err != nil {
-		return statusResponse.Status, err
-	}
+    err = json.Unmarshal(body, &statusResponse)
+    if err != nil {
+        return statusResponse.Status, err
+    }
 
-	return statusResponse.Status, nil
+    return statusResponse.Status, nil
 }
 
 func Authenticate(host string) (token string, err error) {
-	resp, err := http.Get("http://" + host + ":" + strconv.Itoa(Port) + "/authenticate")
-	if err != nil {
-		return token, err
-	}
+    resp, err := http.Get("http://" + host + ":" + strconv.Itoa(Port) + "/authenticate")
+    if err != nil {
+        return token, err
+    }
 
-	if resp.StatusCode == http.StatusForbidden {
-		return token, errors.New("make sure mutesync allows external apps")
-	}
+    if resp.StatusCode == http.StatusForbidden {
+        return token, errors.New("make sure mutesync allows external apps")
+    }
 
-	if resp.StatusCode != http.StatusOK {
-		return token, errors.New("unexpected response from mutesync")
-	}
+    if resp.StatusCode != http.StatusOK {
+        return token, errors.New("unexpected response from mutesync")
+    }
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return token, err
-	}
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        return token, err
+    }
 
-	var authResponse authResponse
-	err = json.Unmarshal(body, &authResponse)
-	if err != nil {
-		return token, err
-	}
+    var authResponse authResponse
+    err = json.Unmarshal(body, &authResponse)
+    if err != nil {
+        return token, err
+    }
 
-	token = authResponse.Token
+    token = authResponse.Token
 
-	return token, nil
+    return token, nil
 }
--- structs.go
+++ structs.go
@@ -1,16 +1,16 @@
 package mutesync
 
 type authResponse struct {
-	Token string `json:"token"`
+    Token string `json:"token"`
 }
 
 type statusResponse struct {
-	Status Status `json:"data"`
+    Status Status `json:"data"`
 }
 
 type Status struct {
-	InMeeting bool   `json:"in_meeting"`
-	Hostname  string `json:"hostname"`
-	UserID    string `json:"user-id"`
-	Muted     bool   `json:"muted"`
+    InMeeting bool   `json:"in_meeting"`
+    Hostname  string `json:"hostname"`
+    UserID    string `json:"user-id"`
+    Muted     bool   `json:"muted"`
 }
